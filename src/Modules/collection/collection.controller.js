1245dedb3b84d3e75c63e1a9f3fb7b7bd0f2bf1f
KallMeMoh <justmohammad66@gmail.com>
2026-02-06 21:25:04 +0200

    Implement: /collection/books endpoint.

--- src/Modules/collection/collection.controller.js
+++ src/Modules/collection/collection.controller.js
@@ -1,13 +1,26 @@
 import { Router } from 'express';
+import { db } from '../../DB/connection.js';
 
 const collectionRouter = Router();
 
-collectionRouter.post('/books');
+collectionRouter.post('/books', async (req, res, next) => {
+  try {
+    await db.createCollection('books', {
+      validator: {
+        title: { $exists: true, $ne: '' },
+      },
+    });
 
-collectionRouter.post('/authors');
+    return res.status(200).json({ ok: 1 });
+  } catch (error) {
+    next(error);
+  }
+});
 
-collectionRouter.post('/logs/capped');
+collectionRouter.post('/authors', (req, res, next) => {});
 
-collectionRouter.post('/books/index');
+collectionRouter.post('/logs/capped', (req, res) => {});
+
+collectionRouter.post('/books/index', (req, res) => {});
 
 export default collectionRouter;
